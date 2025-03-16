5b87916ea0f2af07e97b8fdf981a1335b21af357
Dhruv-Gaba <141745373+Dhruv-Gaba@users.noreply.github.com>
2025-03-16 23:02:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,113 +1,160 @@
-let words=document.querySelectorAll(".word");
-words.forEach((word)=>{
-    let letters=word.textContent.split("");
-    word.textContent="";
-    letters.forEach((letter)=>{
-        let span=document.createElement("span");
-        span.textContent=letter;
-        span.className="letter";
-        word.append(span);
-    });
+let words = document.querySelectorAll(".word");
+words.forEach((word) => {
+  let letters = word.textContent.split("");
+  word.textContent = "";
+  letters.forEach((letter) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.className = "letter";
+    word.append(span);
+  });
 });
 
-let currentWordIndex=0;
-let maxWordIndex=words.length-1;
-words[currentWordIndex].style.opacity="1";
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
 
-let changeText=()=>{
-    let currentWord=words[currentWordIndex];
-    let nextWord=currentWordIndex===maxWordIndex?words[0]:words[currentWordIndex+1];
+let changeText = () => {
+  let currentWord = words[currentWordIndex];
+  let nextWord =
+    currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
 
-    Array.from(currentWord.children).forEach((letter,i)=>{
-        setTimeout(()=>{
-            letter.className="letter out";
-        },i*80);
-    });
-    nextWord.style.opacity="1";
-    Array.from(nextWord.children).forEach((letter,i)=>{
-        letter.className="letter behind";
-        setTimeout(()=>{
-            letter.className="letter in";
-        },340+i*80);
-    });
-    currentWordIndex=currentWordIndex===maxWordIndex?0:currentWordIndex+1;
+  Array.from(currentWord.children).forEach((letter, i) => {
+    setTimeout(() => {
+      letter.className = "letter out";
+    }, i * 80);
+  });
+  nextWord.style.opacity = "1";
+  Array.from(nextWord.children).forEach((letter, i) => {
+    letter.className = "letter behind";
+    setTimeout(() => {
+      letter.className = "letter in";
+    }, 340 + i * 80);
+  });
+  currentWordIndex =
+    currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
 };
 changeText();
-setInterval(changeText,3000);
+setInterval(changeText, 3000);
 
 //circle skill///////////////////////////////
 
-const circles=document.querySelectorAll('.circle');
-circles.forEach(elem=>{
-    var dots=elem.getAttribute("data-dots");
-    var marked = elem.getAttribute("data-percent");
-    var percent=Math.floor(dots*marked/100);
-    var points="";
-    var rotate=360/dots;
+const circles = document.querySelectorAll(".circle");
+circles.forEach((elem) => {
+  var dots = elem.getAttribute("data-dots");
+  var marked = elem.getAttribute("data-percent");
+  var percent = Math.floor((dots * marked) / 100);
+  var points = "";
+  var rotate = 360 / dots;
 
+  for (let i = 0; i < dots; i++) {
+    points += ` <div class="points" style="--i:${i};--rot:${rotate}deg"></div>`;
+  }
+  elem.innerHTML = points;
 
-    for(let i=0;i<dots;i++){
-        points+=` <div class="points" style="--i:${i};--rot:${rotate}deg"></div>`;
-    }
-    elem.innerHTML=points;
-
-    const pointsMarked=elem.querySelectorAll('.points');
-    for(let i=0;i<percent;i++){
-        pointsMarked[i].classList.add('marked');
-    }
-})
+  const pointsMarked = elem.querySelectorAll(".points");
+  for (let i = 0; i < percent; i++) {
+    pointsMarked[i].classList.add("marked");
+  }
+});
 
 //active menu
-let menuLi=document.querySelectorAll('header ul li a');
-let section=document.querySelectorAll('section');
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll("section");
 
-function activeMenu(){
-    let len=section.length;
-    while(--len && window.scrollY + 97 < section[len].offsetTop){}
-    menuLi.forEach(sec=>sec.classList.remove("active"));
-    menuLi[len].classList.add("active");
+function activeMenu() {
+  let len = section.length;
+  while (--len && window.scrollY + 97 < section[len].offsetTop) {}
+  menuLi.forEach((sec) => sec.classList.remove("active"));
+  menuLi[len].classList.add("active");
 }
 
 activeMenu();
-window.addEventListener("scroll",activeMenu);
+window.addEventListener("scroll", activeMenu);
 
 //sticky navbar
-const header=document.querySelector("header");
-window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky",window.scrollY>50);
-})
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 50);
+});
 
 //toggle icon navbar
-let menuIcon=document.querySelector("#menu-icon");
+let menuIcon = document.querySelector("#menu-icon");
 let navlist = document.querySelector(".navlist");
 
-menuIcon.onclick=()=>{
-    menuIcon.classList.toggle("bx-x");
-    navlist.classList.toggle("open");
-}
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
 
-window.onscroll=()=>{
-    menuIcon.classList.remove("bx-x");
-    navlist.classList.remove("open");
-}
+window.onscroll = () => {
+  menuIcon.classList.remove("bx-x");
+  navlist.classList.remove("open");
+};
 
 //parallax
 
-const observer=new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        if(entry.isIntersecting){
-            entry.target.classList.add("show-items");
-        }else{
-            entry.target.classList.remove("show-items");
-        }
-    });
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show-items");
+    } else {
+      entry.target.classList.remove("show-items");
+    }
+  });
 });
 
-const scrollScale=document.querySelectorAll(".scroll-scale");
-scrollScale.forEach((el)=>observer.observe(el));
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el) => observer.observe(el));
 
-const scrollBottom=document.querySelectorAll(".scroll-bottom");
-scrollBottom.forEach((el)=>observer.observe(el));
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el) => observer.observe(el));
 
-const scrollTop=document.querySelectorAll(".scroll-top");
-scrollTop.forEach((el)=>observer.observe(el));
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el) => observer.observe(el));
+
+//form validation
+document
+  .querySelector(".contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let isValid = true;
+
+    // Name Validation
+    let name = document.querySelector("#name").value.trim();
+    if (name === "") {
+      document.getElementById("nameError").innerText = "*Name is required";
+      document.getElementById("nameError").style.color = "red";
+      isValid = false;
+    } else {
+      document.getElementById("nameError").innerText = "";
+    }
+
+    // Email Validation
+    let email = document.querySelector("#email").value.trim();
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!email.match(emailPattern)) {
+      document.getElementById("emailError").innerText = "*Invalid email format";
+      document.getElementById("emailError").style.color = "red";
+      isValid = false;
+    } else {
+      document.getElementById("emailError").innerText = "";
+    }
+
+    // phone no. Validation (10 characters)
+    let phone = document.querySelector("#phone").value.trim();
+    if (phone.length < 10 || phone.length > 10) {
+      document.getElementById("phoneError").innerText =
+        "*enter a valid phone number";
+      document.getElementById("phoneError").style.color = "red";
+      isValid = false;
+    } else {
+      document.getElementById("phoneError").innerText = "";
+    }
+
+    if (isValid) {
+      alert("Form submitted successfully!");
+      document.querySelector(".contact-form").reset(); // Reset form fields
+    }
+  });
